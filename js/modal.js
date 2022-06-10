78f208131ea9eb8348d1ed30775b5ddac67867d3
vokur13 <vokur13@gmail.com>
2022-06-10 18:17:43 +0300

    home page adaptive layout fixed

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,3 @@
-// Header positioning
-
-const { height: pageHeaderHeight } = document.querySelector('.header').getBoundingClientRect();
-
-document.body.style.paddingTop = `${pageHeaderHeight}px`;
-
 // Dropbox Modal
 
 const refs = {
